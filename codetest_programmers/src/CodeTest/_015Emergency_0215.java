2d32bc5dde1b2747dccfceadbfc721da46756855
HyeongChank <jsk381988@gmail.com>
2023-03-27 09:05:41 +0900

    update

---
+++ codetest_programmers/src/CodeTest/_015Emergency_0215.java
@@ -0,0 +1,33 @@
+package CodeTest;
+
+public class _015Emergency_0215 {
+
+	public static void main(String[] args) {
+		int emergency[] = {3,76,24};
+		int [] answer = new int[emergency.length];
+		int [] a1 = new int[emergency.length];
+		float [] a2 = new float[emergency.length];
+		for(int i =0;i< emergency.length;i++) {
+			a1[i] = i;
+			a2[i] = emergency[i];
+		}
+		int [] e2 = emergency;
+		
+		int min =100;
+		float temp =1;
+		int locate =0;
+		for(int i=0; i<emergency.length;i++) {
+			for(int j =0; j<emergency.length;j++) {
+				if(emergency[j]<min) {
+					locate = j;
+				}
+			}
+	
+			
+			answer[locate] =0;
+			temp++;
+			
+		}
+	}
+
+}
